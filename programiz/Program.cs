07a1ae5c39056a400c44569c41c3446726c1d653
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-11-11 05:44:58 -0500

    hello world
    understanding c# with programiz

---
+++ programiz/Program.cs
@@ -0,0 +1,12 @@
+﻿// C# Hello World - Your First C# Program
+namespace HelloWorld // defining a namespace
+{
+    class Hello // defining a class
+    {
+        static void Main(string[] args) // main method of the program
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
+// reference: https://www.programiz.com/csharp-programming/hello-world
