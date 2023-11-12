000a53d7faddd3a7e115572b03809784655d249e
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-11-12 15:44:34 -0500

    operators

--- programiz/Program.cs
+++ programiz/Program.cs
@@ -1,38 +1,105 @@
 ﻿// C# Variables and (Primitive) Data Types
 using System;
 
-namespace DataType // defining a namespace
+namespace Operator // defining a namespace
 {
     class Example // defining a class
     {
         static void Main(string[] args) // main method of the program
         {
-            sbyte num1 = 127; // declaring and initialiazing a variable
-            short num2 = 32767; // '32767' is a literal expression
-            int num3 = -51793;
-            long number1 = -8445482L; // 'long' is a keyword, 'number' is an identifier
-            byte num4 = 0; // all the not decimal numbers from here can be only greather than 0 (unsigned)
-            ushort num5 = 64121;
-            uint num6 = 705641;
-            ulong number2 = 97844121L;
-            float decimal1 = 110.45F;
-            double decimal2 = -48721.43D;
-            char character = 'a';
-            string phrase = "we live we love we lie";
-            decimal money = 101.15M;
-            Console.WriteLine("sbyte => value(num1): " + num1);
-            Console.WriteLine("short => value(num2): " + num2);
-            Console.WriteLine("int => value(num3): " + num3);
-            Console.WriteLine("long => value(number1): " + number1);
-            Console.WriteLine("byte => value(num4): " + num4);
-            Console.WriteLine("ushort => value(num5): " + num5);
-            Console.WriteLine("uint => value(num6): " + num6);
-            Console.WriteLine("ulong => value(number2): " + number2);
-            Console.WriteLine("float => value(decimal1): " + decimal1);
-            Console.WriteLine("double => value(decimal2): " + decimal2);
-            Console.WriteLine("char => value(character): " + character);
-            Console.WriteLine("string => value(phrase): " + phrase);
-            Console.WriteLine("decimal => value(money): " + money);
+            double dec1 = 15.31D, dec2 = 8.05D, result;
+            int int1 = 15, int2 = 6, mod;
+
+            /* ARITHMETIC OPERATORS */
+
+            // addition
+            result = dec1 + dec2;
+            Console.WriteLine("{0} + {1} = {2}", dec1, dec2, result);
+
+            // substraction
+            result = dec1 - dec2;
+            Console.WriteLine("{0} - {1} = {2}", dec1, dec2, result);
+
+            // multiplication
+            result = dec1 * dec2;
+            Console.WriteLine("{0} * {1} = {2}", dec1, dec2, result);
+
+            // division
+            result = dec1 / dec2;
+            Console.WriteLine("{0} / {1} = {2}", dec1, dec2, result);
+
+            // module
+            mod = int1 % int2;
+            Console.WriteLine("{0} % {1} = {2}", int1, int2, mod);
+
+            /* RELATIONAL OPERATOR */
+            bool value;
+            int val1 = 4, val2 = 3;
+
+            // equal to
+            value = (val1 == val2);
+            Console.WriteLine("{0} == {1} -> {2}", val1, val2, value);
+
+            // equal to
+            value = (val1 != val2);
+            Console.WriteLine("{0} != {1} -> {2}", val1, val2, value);
+
+            // greather than
+            value = (val1 > val2);
+            Console.WriteLine("{0} > {1} -> {2}", val1, val2, value);
+
+            // greather than or equal to
+            value = (val1 >= val2);
+            Console.WriteLine("{0} >= {1} -> {2}", val1, val2, value);
+
+            // less than
+            value = (val1 < val2);
+            Console.WriteLine("{0} < {1} -> {2}", val1, val2, value);
+
+            // less than or equal to
+            value = (val1 <= val2);
+            Console.WriteLine("{0} <= {1} -> {2}", val1, val2, value);
+
+            /* LOGICAL OPERATORS */
+            bool flag1 = true, flag2 = false;
+
+            // and
+            value = flag1 && flag2;
+            Console.WriteLine("{0} && {1} -> {2}", flag1, flag2, value); 
+
+            // or
+            value = flag1 || flag2;
+            Console.WriteLine("{0} || {1} -> {2}", flag1, flag2, value);
+
+            /* UNARY OPERATORS */
+            int age = 15, falseAge = -25;
+
+            // unary plus
+            age = +falseAge;
+            Console.WriteLine("unary plus -> {0}", age);
+
+            // unary minus
+            age = -falseAge;
+            Console.WriteLine("unary plus -> {0}", age);
+
+            // increment
+            ++age; // is equal to 'age = ++age'
+            Console.WriteLine("increment -> {0}", age);
+
+            // decrement
+            --age; // 'age--' works totally different, the change is seen in the next line
+            Console.WriteLine("decrement -> {0}", age);
+
+            // logical negation
+            value = !(age == falseAge);
+            Console.WriteLine("not -> {0}", value);
+
+            /* TERNARY OPERATOR */
+            bool flag = (falseAge > 0 ? true : false);
+
+            Console.WriteLine("ternary -> {0}", flag);
+
+            /* There are also bitwise, bitshift and compound operators */
         }
     }
 }
