39202caf863264787dffbb9115c2ab8d6b5f3e63
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-11-12 16:53:05 -0500

    w3resource [basic exercises] - 5

--- w3resource/basicexercises/exercises.cs
+++ w3resource/basicexercises/exercises.cs
@@ -1,16 +1,12 @@
 /*
-4. Write a C# Sharp program to print the results of the specified operations.
-Test data:
-
--1 + 4 * 6
-( 35+ 5 ) % 7
-14 + -4 * 6 / 11
-2 + 15 / 6 * 1 - 7 % 2
+5. Write a C# Sharp program to swap two numbers.
+Test Data:
+Input the First Number : 5
+Input the Second Number : 6
 Expected Output:
-23
-5
-12
-3
+After Swapping :
+First Number : 6
+Second Number : 5
 */
 namespace Exercises
 {
@@ -40,6 +36,22 @@ namespace Exercises
             System.Console.WriteLine($"{(35 + 5) % 7}");
             System.Console.WriteLine($"{14 + -4 * 6 / 11}");
             System.Console.WriteLine($"{2 + 15 / 6 * 1 - 7 % 2}");
+        }
+
+        public static void SolveExercise5()
+        {
+            Console.Write("Input the First Number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.Write("Input the Second Number: ");
+            int num2 = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Before swapping");
+            System.Console.WriteLine($"num1 = {num1}, num2 = {num2}");
+            int aux = num1;
+            num1 = num2;
+            num2 = aux;
+            System.Console.WriteLine("After swapping");
+            System.Console.WriteLine($"num1 = {num1}, num2 = {num2}");
+
         }
     }
 }
