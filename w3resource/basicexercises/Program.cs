f7a64ccc48c305b0eabbb2d7d16995a8fe9d74cd
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-11-12 16:32:26 -0500

    w3resource [basic exercises] - 1

---
+++ w3resource/basicexercises/Program.cs
@@ -0,0 +1,15 @@
+﻿// C# Sharp Basic Declarations and Expressions : Exercises, Practice, Solution
+// C# Sharp Basic Exercises [104 exercises with solution]
+// reference: https://www.w3resource.com/csharp-exercises/basic/index.php
+using Exercises;
+
+namespace W3Resource
+{
+    class BasicExercises
+    {
+        public static void Main(string[] args)
+        {
+            Solution.SolveExercise1();
+        }
+    }
+}
---
+++ w3resource/basicexercises/exercises.cs
@@ -0,0 +1,16 @@
+/*
+1. Write a C# Sharp program to print Hello and your name in a separate line.
+Expected Output :
+Hello: Alexandra Abramov
+*/
+namespace Exercises
+{
+    class Solution
+    {
+        public static void SolveExercise1()
+        {
+            var myName = "Juan";
+            System.Console.WriteLine("Hello:\n" + myName);
+        }
+    }
+}
