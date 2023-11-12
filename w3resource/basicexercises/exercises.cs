87e26f02348bf8e6823976dc35e52ec479d3d8e6
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-11-12 16:46:17 -0500

    w3resource [basic exercises] - 4

--- w3resource/basicexercises/exercises.cs
+++ w3resource/basicexercises/exercises.cs
@@ -1,5 +1,16 @@
 /*
-3. Write a C# Sharp program to print the result of dividing two numbers.
+4. Write a C# Sharp program to print the results of the specified operations.
+Test data:
+
+-1 + 4 * 6
+( 35+ 5 ) % 7
+14 + -4 * 6 / 11
+2 + 15 / 6 * 1 - 7 % 2
+Expected Output:
+23
+5
+12
+3
 */
 namespace Exercises
 {
@@ -21,6 +32,14 @@ namespace Exercises
         {
             double a = 11D, b = 2, res = a / b;
             System.Console.WriteLine("div: {0} / {1} = {2}", a, b, res);
+        }
+
+        public static void SolveExercise4()
+        {
+            System.Console.WriteLine($"{-1 + 4 * 6}");
+            System.Console.WriteLine($"{(35 + 5) % 7}");
+            System.Console.WriteLine($"{14 + -4 * 6 / 11}");
+            System.Console.WriteLine($"{2 + 15 / 6 * 1 - 7 % 2}");
         }
     }
 }
