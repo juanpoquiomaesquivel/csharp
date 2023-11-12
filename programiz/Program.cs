f613a997f8b322d7a8d28544bb826b40f8036011
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-11-12 12:58:39 -0500

    keywords and identifiers

--- programiz/Program.cs
+++ programiz/Program.cs
@@ -1,12 +1,15 @@
-﻿// C# Hello World - Your First C# Program
+﻿// C# Keywords and Identifiers
+using System;
+
 namespace HelloWorld // defining a namespace
 {
     class Hello // defining a class
     {
         static void Main(string[] args) // main method of the program
         {
-            System.Console.WriteLine("Hello World!");
+            long number = 45; // 'long' is a keyword, 'number' is an identifier
+            Console.WriteLine("value(number): " + number);
         }
     }
 }
-// reference: https://www.programiz.com/csharp-programming/hello-world
+// reference: https://www.programiz.com/csharp-programming/keywords-identifiers
