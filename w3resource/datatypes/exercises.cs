7e4cfb9e8bec93f18f9c20f0d48ca9f557421f59
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-11-13 02:57:16 -0500

    w3resource [data types] - 2

--- w3resource/datatypes/exercises.cs
+++ w3resource/datatypes/exercises.cs
@@ -1,11 +1,17 @@
 /*
-1. Write a C# Sharp program that takes three letters and displays them in reverse order.
+2. Write a C# Sharp program that takes a number and a width also a number.
+It then displays a triangle of that width using that number.
 Test Data
-Enter letter: b
-Enter letter: a
-Enter letter: t
+Enter a number: 6
+Enter the desired width: 6
 Expected Output :
-t a b
+
+666666
+66666
+6666
+666
+66
+6
 */
 namespace Exercises
 {
@@ -13,7 +19,9 @@ namespace Exercises
     {
         public static void SolveExercise1()
         {
-            char letter1, letter2, letter3;
+            char letter1,
+                letter2,
+                letter3;
             System.Console.Write("Enter letter [1]: ");
             letter1 = Convert.ToChar(System.Console.ReadLine()!);
             System.Console.Write("Enter letter [2]: ");
@@ -21,6 +29,24 @@ namespace Exercises
             System.Console.Write("Enter letter [3]: ");
             letter3 = Convert.ToChar(System.Console.ReadLine()!);
             System.Console.Write("{0} {1} {2}", letter3, letter2, letter1);
+        }
+
+        public static void SolveExercise2()
+        {
+            int number,
+                width;
+            System.Console.Write("Enter a number: ");
+            number = int.Parse(System.Console.ReadLine()!);
+            System.Console.Write("Enter the desired width: ");
+            width = int.Parse(System.Console.ReadLine()!);
+
+            for (int i = width; i >= 1; i--)
+            {
+                for (int j = i; j >= 1; j--)
+                    System.Console.Write(number);
+
+                System.Console.WriteLine();
+            }
         }
     }
 }
