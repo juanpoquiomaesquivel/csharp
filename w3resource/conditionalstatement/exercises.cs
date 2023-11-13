34b5a1bba8308349b84ecef7c836c2e1b250f6e6
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-11-13 03:10:33 -0500

    w3resource [conditional statement] - 2

--- w3resource/conditionalstatement/exercises.cs
+++ w3resource/conditionalstatement/exercises.cs
@@ -1,10 +1,8 @@
 /*
-1. Write a C# Sharp program to accept two integers and check whether they are equal or not.
-Test Data :
-Input 1st number: 5
-Input 2nd number: 5
+2. Write a C# Sharp program to check whether a given number is even or odd.
+Test Data : 15
 Expected Output :
-5 and 5 are equal
+15 is an odd integer
 */
 namespace Exercises
 {
@@ -19,6 +17,14 @@ namespace Exercises
             System.Console.Write("Enter a second number: ");
             y = Convert.ToInt32(System.Console.ReadLine());
             System.Console.Write("{0} and {1} are {2}", x, y, x == y ? "equal." : "not equal.");
+        }
+
+        public static void SolveExercise2()
+        {
+            int num;
+            System.Console.Write("Enter the number: ");
+            num = Convert.ToInt32(System.Console.ReadLine());
+            System.Console.Write("{0} is an {1} integer", num, num % 2 == 0 ? "even" : "odd");
         }
     }
 }
