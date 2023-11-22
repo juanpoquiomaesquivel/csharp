4eaaf569782649490c5a11ac1be9460867318906
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-11-22 04:15:37 -0500

    w3resource [function] - 1

---
+++ w3resource/function/Program.cs
@@ -0,0 +1,15 @@
+﻿// C# Sharp String : Exercises, Practice, Solution
+// C# Sharp String [68 exercises with solution]
+// reference: https://www.w3resource.com/csharp-exercises/string/index.php
+using Exercises;
+
+namespace W3Resource
+{
+    class Function
+    {
+        public static void Main(string[] args)
+        {
+            Solution.SolveExercise1();
+        }
+    }
+}
---
+++ w3resource/function/exercises.cs
@@ -0,0 +1,22 @@
+/*
+1. Write a program in C# Sharp to create a user defined function.
+Expected Output :
+Welcome Friends!
+Have a nice day!
+*/
+namespace Exercises
+{
+    class Solution
+    {
+        public static void SolveExercise1()
+        {
+            System.Console.WriteLine("THIS IS AN EXAMPLE OF AN USER DEFINED FUNCTION!");
+            _exe1_Welcome();
+        }
+
+        private static void _exe1_Welcome()
+        {
+            System.Console.WriteLine("Welcome To This World!");
+        }
+    }
+}
