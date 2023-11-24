7256ae976e5e9f766c1af4ec752a20f723328234
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-11-24 15:18:30 -0500

    w3resource [recursion] - 2

--- w3resource/recursion/exercises.cs
+++ w3resource/recursion/exercises.cs
@@ -1,9 +1,9 @@
 /*
-1. Write a program in C# Sharp to print the first n natural numbers using recursion.
+2. Write a program in C# Sharp to print numbers from n to 1 using recursion.
 Test Data :
 How many numbers to print : 10
 Expected Output :
-1 2 3 4 5 6 7 8 9 10
+10 9 8 7 6 5 4 3 2 1
 */
 namespace Exercises
 {
@@ -22,6 +22,22 @@ namespace Exercises
             {
                 _exe1_printNumber(n - 1);
                 System.Console.Write(n + " ");
+            }
+        }
+
+        public static void SolveExercise2()
+        {
+            System.Console.Write("How many numbers to print: ");
+            int number = Convert.ToInt32(System.Console.ReadLine()!);
+            _exe2_printNumber(number);
+        }
+
+        private static void _exe2_printNumber(int n)
+        {
+            if (n >= 1)
+            {
+                System.Console.Write(n + " ");
+                _exe2_printNumber(n - 1);
             }
         }
     }
