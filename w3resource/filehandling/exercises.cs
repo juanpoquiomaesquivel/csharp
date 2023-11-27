65ab189072a4f625ca3bdc00a361e2d196760449
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-11-27 04:56:55 -0500

    w3resource [file handling] - 2

--- w3resource/filehandling/exercises.cs
+++ w3resource/filehandling/exercises.cs
@@ -1,8 +1,13 @@
 /*
-1. Write a program in C# Sharp to create a blank file on the disk.
+2. Write a program in C# Sharp to remove a file from the disk.
 Expected Output :
-
- A file created with name mytest.txt
+ Remove a file from the disk (at first create the file ) :
+--------------------------------------------------------------
+ The file mytest.txt deleted successfully.
+ 
+  Remove a file from the disk (at first create the file ) :
+--------------------------------------------------------------
+ File does not exist
 */
 using System;
 
@@ -24,6 +29,22 @@ namespace Exercises
             catch (System.Exception e)
             {
                 Console.WriteLine(e.ToString());
+            }
+        }
+
+        public static void SolveExercise2()
+        {
+            string fileName = @"mytest.txt";
+
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+                Console.WriteLine("The file {0} has been deleted successfully.", fileName);
+            }
+            else
+            {
+                Console.Write("The file does not exist. [PRESS TO CONTINUE]");
+                Console.ReadKey();
             }
         }
     }
