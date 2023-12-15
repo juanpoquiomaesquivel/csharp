7da563719b8269ccb208c3f9a51ccb318d292a01
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-12-15 03:09:49 -0500

    w3resource [conditional statement] - 6

--- w3resource/conditionalstatement/exercises.cs
+++ w3resource/conditionalstatement/exercises.cs
@@ -1,9 +1,9 @@
 /*
-5. Write a C# Sharp program to read the age of a candidate and determine whether it is eligible 
-for casting his/her own vote.
-Test Data : 21
+6. Write a C# Sharp program to read the value of an integer m and display the value of n is
+1 when m is larger than 0, 0 when m is 0 and -1 when m is less than 0.
+Test Data : -5
 Expected Output:
-Congratulation! You are eligible for casting your vote.
+The value of n = -1
 */
 using System;
 
@@ -54,12 +54,27 @@ namespace Exercises
             Console.Write("Enter the age of the candidate: ");
             int age = Convert.ToInt32(Console.ReadLine());
             bool isEligible = age >= 18;
-            
+
             if (isEligible)
                 Console.WriteLine("Congratulations! you are eligible for casting your vote.");
             else
-                Console.WriteLine("Sorry! you are not eligible for this election, you're underage.");
+                Console.WriteLine(
+                    "Sorry! you are not eligible for this election, you're underage."
+                );
+        }
 
+        public static void SolveExercise6()
+        {
+            Console.Write("Enter the 'm' value: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            int n = -1;
+
+            if (m > 0)
+                n = 1;
+            else if (m == 0)
+                n = 0;
+
+            Console.WriteLine("The value of 'n' is {0}.", n);
         }
     }
 }
