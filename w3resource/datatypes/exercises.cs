c82c793809b7fc49d311a05145a65f8f20b5cf22
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-12-27 06:01:34 -0500

    w3resource [data types] - 7

--- w3resource/datatypes/exercises.cs
+++ w3resource/datatypes/exercises.cs
@@ -1,6 +1,15 @@
 /*
-6. Write a C# Sharp program to display certain values of the function x = y2 + 2y + 1 
-(using integer numbers for y, ranging from -5 to +5).
+7. Write a C# Sharp program that takes distance and time as input and
+displays speed in kilometres per hour and miles per hour.
+Test Data:
+Input distance(metres): 50000
+Input timeSec(hour): 1
+Input timeSec(minutes): 35
+Input timeSec(seconds): 56
+Expected Output:
+Your speed in metres/sec is 8.686588
+Your speed in km/h is 31.27172
+Your speed in miles/h is 19.4355
 */
 namespace Exercises
 {
@@ -124,6 +133,32 @@ namespace Exercises
                 x = Math.Pow(y, 2) + 2 * y + 1;
                 Console.WriteLine("y = {0}, x = ({0})² + 2 * {0} + 1 = {1}", y, x);
             }
+        }
+
+        public static void SolveExercise7()
+        {
+            float dMeters,
+                tHours,
+                tMinutes,
+                tSeconds;
+            Console.Write("Enter the distance (in meters): ");
+            dMeters = Convert.ToSingle(Console.ReadLine()!);
+            Console.Write("Enter the time (in hours): ");
+            tHours = Convert.ToSingle(Console.ReadLine()!);
+            Console.Write("Enter the time (in minutes): ");
+            tMinutes = Convert.ToSingle(Console.ReadLine()!);
+            Console.Write("Enter the time (in seconds): ");
+            tSeconds = Convert.ToSingle(Console.ReadLine()!);
+
+            float v1,
+                v2,
+                v3;
+            v1 = dMeters / (tHours * 3600 + tMinutes * 60 + tSeconds);
+            v2 = (dMeters / 1000f) / (tHours + (tMinutes / 60) + (tSeconds / 3600f));
+            v3 = (dMeters / 1609.34f) / (tHours + (tMinutes / 60) + (tSeconds / 3600f));
+            Console.WriteLine("Your speed in m/s is {0}", v1);
+            Console.WriteLine("Your speed in km/h is {0}", v2);
+            Console.WriteLine("Your speed in mil/h is {0}", v3);
         }
     }
 }
