1e81d3f19c6329c05f4b2cff925516145b76667d
juanpoquiomaesquivel <juanpoquiomaesquivel@hotmail.com>
2023-12-29 08:53:55 -0500

    w3resource [for loop] - 7

--- w3resource/forloop/exercises.cs
+++ w3resource/forloop/exercises.cs
@@ -1,12 +1,12 @@
 /*
-6. Write a program in C# Sharp to display the multiplication table of a given integer.
+7. Write a program in C# Sharp to display the multiplication table vertically from 1 to n.
 Test Data :
-Input the number (Table to be calculated) : 15
+Input upto the table number starting from 1 : 8
 Expected Output :
-15 X 1 = 15
+Multiplication table from 1 to 8
+1x1 = 1, 2x1 = 2, 3x1 = 3, 4x1 = 4, 5x1 = 5, 6x1 = 6, 7x1 = 7, 8x1 = 8
 ...
-...
-15 X 10 = 150
+1x10 = 10, 2x10 = 20, 3x10 = 30, 4x10 = 40, 5x10 = 50, 6x10 = 60, 7x10 = 70, 8x10 = 80
 */
 using System;
 
@@ -80,6 +80,16 @@ namespace Exercises
 
             for (int i = 0; i <= 10; i++)
                 Console.WriteLine("{0} x {1} = {2}", num, i, num * i);
+        }
+
+        public static void SolveExercise7()
+        {
+            Console.Write("Enter the value of 'n': ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            for (int j = 0; j <= 10; j++)
+                Console.WriteLine("{0} x {1} = {2}", i, j, i * j);
         }
     }
 }
